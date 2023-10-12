4029a072afde851ebb4808663571ae880e484ba3
itohara22 <ito.k23018@gmail.com>
2023-10-12 23:26:55 +0530

    comments

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,21 +8,21 @@ import TopCards from "@/app/components/TopCard";
 import DoughnutChart from "@/app/components/charts/DougnutChart";
 
 export default async function Dashboard() {
-  const apiPrefix = process.env.NEXT_PUBLIC_API_PREFIX ?? "http://";
-  const apiHost = process.env.NEXT_PUBLIC_VERCEL_URL ?? "localhost:3000";
+  // const apiPrefix = process.env.NEXT_PUBLIC_API_PREFIX ?? "http://";
+  // const apiHost = process.env.NEXT_PUBLIC_VERCEL_URL ?? "localhost:3000";
 
-  const productUrl = apiPrefix + apiHost + "/api/products";
-  const userUrl = apiPrefix + apiHost + "/api/users";
+  // const productUrl = apiPrefix + apiHost + "/api/products";
+  // const userUrl = apiPrefix + apiHost + "/api/users";
 
-  const res = await fetch(productUrl, {
-    cache: "no-store"
-  }).then((data) => {
-    return data.json();
-  });
+  // const res = await fetch(productUrl, {
+  //   cache: "no-store"
+  // }).then((data) => {
+  //   return data.json();
+  // });
 
-  const users = await fetch(userUrl, {
-    cache: "no-store"
-  }).then((data) => data.json());
+  // const users = await fetch(userUrl, {
+  //   cache: "no-store"
+  // }).then((data) => data.json());
 
   return (
     <div className="min-h-screen bg-[#F8FAFF]">
@@ -30,11 +30,11 @@ export default async function Dashboard() {
       <div className="md:ml-[min(25vw,280px)]  h-full">
         <Header />
         <TopCards />
-        <ActivityChart users={users} />
+        {/* <ActivityChart users={users} /> */}
         <div className="mx-auto mt-4 mb-4 w-[90%] max-w-[1050px] grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4">
           <div className="bg-white p-2 rounded-lg drop-shadow-lg">
             <h2 className="text-lg font-semibold">Top Products</h2>
-            <DoughnutChart res={res} />
+            {/* <DoughnutChart res={res} /> */}
           </div>
           <AddUser />
         </div>
